0a2e5ffd0f2c52110f3d8a0d4b4be7451f4dd3ba
hi1004 <hi1004@github.com>
2021-07-14 14:50:40 +0900

    수정

--- src/App.vue
+++ src/App.vue
@@ -12,17 +12,20 @@ import Header from '~/components/Header'
 import Totop from '~/components/Totop'
 import Footer from '~/components/Footer'
 import Canvas from '~/components/Canvas'
+
 export default {
   components: {
     Header,
     Canvas,
     Totop,
     Footer,
-  }
+  },
+
 }
 </script>
 
 
 <style lang="scss">
   @import "~/scss/main";
+ 
 </style>
