403d209148c13b20e9c1e3fed6ab745d40ba6db7
DongHun, Kim <donghun.kdh@gmail.com>
2021-07-16 14:26:21 +0900

    revise black background fade out effect

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -2,7 +2,6 @@
   <div
     class="intro-stage"
     data-depth="1">
-    <div class="black"></div>
     <div class="ab main">
       <img
         src="../assets/Intro/m_J.png"
@@ -88,17 +87,6 @@
     width: 100vw;
     height: 100vh;    
     z-index: -1;
-    .black{
-      position: fixed;
-      width: 100vw;
-      height: 100vh;
-      transition: 1s;
-      // z-index: 9;
-      &.hidden-black{
-        opacity: 0;
-        z-index: 0;
-      }
-    }  
     .ab{
       position: absolute;
       top: 50%;
