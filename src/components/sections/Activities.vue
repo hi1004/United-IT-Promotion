0da572c0c0d0990a64960ab5e651485f1b6cfcb1
hi1004 <hi1004@github.com>
2021-08-11 01:20:15 +0900

    activities first section design

--- src/components/sections/Activities.vue
+++ src/components/sections/Activities.vue
@@ -5,9 +5,15 @@
     <div id="activities__container"> 
       <div class="slide__wrap">
         <section class="slide__content first">
-          <h1 class="slide__title">
-            First
-          </h1>
+          <div class="banner">
+            <div class="content">
+              <h1 class="slide__title">
+                개발자가 되기 위한 <span>자바/웹</span> 통합 스터디
+              </h1>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis corrupti omnis vel suscipit odit quod temporibus nihil mollitia, corporis est deleniti repellat saepe sunt voluptatibus obcaecati dolorum nemo, ut rerum.</p>
+              <div class="slide__animation"></div>
+            </div>
+          </div>
           <div class="slide__image_section"></div>
         </section>
         <section class="slide__content second">
@@ -90,23 +96,80 @@
       height: 100vh;    
       overflow: hidden;
       z-index: 100;
-      .slide__title {
-        position: absolute;
-        left: 7vw;
-        top: 5vh;
-        color: #000;
-        font-size: 10vw;
+      .banner {
+        position: relative;
+        width: 100%; 
+        min-height: 100vh; 
+        padding: 0 100px;
+        background: url('~/assets/activity/activity_study.jpg');
+        background-position: center;
+        background-size: cover;
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        .content {
+          max-width: 550px;
+          .slide__title {
+        font-weight: 400;
+        font-size: 2.5rem;
+        letter-spacing: 0.1rem;
+        color: #fff;
         font-family: 'Black Han Sans', sans-serif;
+        span {
+          color: #fdc000;
+        }
+        p {
+          font-size: 1.2rem;
+          font-weight: 300;
+          letter-spacing: 0.02rem;
+          color: #fff;
+          margin: 15px 0 35px;
+        }
+        
       }
-      .slide__image_section{
-        width: 70%;
-        height: 150%;
-        background-color: lightgray;
-        position: absolute;
-        top: -5vh;
-        right: -20vw;
-        transform: rotate(10deg);
-      }    
+        }
+      }
+      .slide__animation {
+          position: absolute;
+          bottom: 50px;
+          left: calc(50% - 250px / 2);
+          width: 250px;
+          height: 150px;
+          background: url('~/assets/activity/a1.png');
+          background-size: cover;
+          animation: animate 15s linear infinite;
+        }
+        @keyframes animate {
+          0%, 100% {
+            background-position: top;
+          }
+          45%, 55% {
+            background-position: bottom;
+          }
+        }
+        @media all and (max-width: 991px) {
+          .banner {
+            padding: 100px 50px;
+            .content {
+              .slide__title {
+              font-size: 2rem;
+              letter-spacing: 0.05rem;
+            }
+            p {
+              font-size: 1rem;
+            }
+            .slide__animation {
+              position: relative;
+              bottom: initial;
+              left: 0;
+              width: 250px;
+              height: 150px;
+              margin: 20px 0;
+            }
+            
+          }
+        }
+        }
       &.index {
         background-color: transparent;
       }
